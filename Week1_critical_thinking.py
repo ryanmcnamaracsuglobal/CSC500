163f03dfc5907eb2a94cbc1631ef3991c4c9c641
ryanmcnamaracsuglobal <145198003+ryanmcnamaracsuglobal@users.noreply.github.com>
2023-09-16 05:03:48 -0600

    Add files via upload

---
+++ Week1_critical_thinking.py
@@ -0,0 +1,23 @@
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+
+num1 = float(input("Hello user. What is the value of num1?"))
+num2 = float(input("Hello user. What is the value of num2?"))
+sum1 = num1 + num2
+print(sum1)
+
+num1 = float(input("Hello user. What is the value of num1?"))
+num2 = float(input("Hello user. What is the value of num2?"))
+sum2 = num1 - num2
+print(sum2)
+
+num1 = float(input("Hello user. What is the value of num1?"))
+num2 = float(input("Hello user. What is the value of num2?"))
+sum3 = num1 / num2
+print(sum3)
+
+num1 = float(input("Hello user. What is the value of num1?"))
+num2 = float(input("Hello user. What is the value of num2?"))
+sum4 = num1 * num2
+print(sum4)
